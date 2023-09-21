671da83bc0acb5175044a6280f6c28b10c17b245
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2023-09-21 14:39:17 +0500

    first commit

---
+++ src/pages/HomeV4.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/header/v4/Header";
+import Banner from "../sections/banner/v4/Banner";
+import TokenSalesInfo from "../sections/banner/tokenInfo";
+
+const HomeV4 = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      {/* <TokenSalesInfo/> */}
+    </>
+  );
+};
+
+export default HomeV4;
---
+++ src/sections/banner/tokenInfo.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import "./tokenInfoStyle.css"
+function TokenSalesInfo() {
+  return (
+    <div className='token-sales-info'>
+      <h2>ICO PRESALE:</h2>
+      <ul>
+        <li>Total Supply: 700,000,000</li>
+        <li>ICO Pre Sale Allocation: 10% of Total Supply = 70,000,000</li>
+        <li>Token Sale Phases:</li>
+        <ul>
+          <li>Phase 1: 17,500,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0090</li>
+            <li>Start Date: 9/22/2023</li>
+            <li>End Date: 10/22/2023</li>
+          </ul>
+          <li>Phase 2: 17,500,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0100</li>
+            <li>Start Date: 10/22/2023</li>
+            <li>End Date: 11/22/2023</li>
+          </ul>
+          <li>Phase 3: 17,500,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0200</li>
+            <li>Start Date: 11/22/2023</li>
+            <li>End Date: 12/22/2023</li>
+          </ul>
+          <li>Phase 4: 17,500,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0300</li>
+            <li>Start Date: 12/22/2023</li>
+            <li>End Date: 1/22/2024</li>
+          </ul>
+        </ul>
+      </ul>
+
+      <h2>IDO SALE:</h2>
+      <ul>
+        <li>Total Supply: 700,000,000</li>
+        <li>IDO Sale Allocation: 15% of Total Supply = 105,000,000</li>
+        <li>Token Sale Phases:</li>
+        <ul>
+          <li>Phase 1: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0400</li>
+            <li>Start Date: 1/25/2024</li>
+            <li>End Date: 1/31/2024</li>
+          </ul>
+          <li>Phase 2: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0500</li>
+            <li>Start Date: 2/1/2024</li>
+            <li>End Date: 2/5/2024</li>
+          </ul>
+          <li>Phase 3: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0600</li>
+            <li>Start Date: 2/6/2024</li>
+            <li>End Date: 2/10/2024</li>
+          </ul>
+        </ul>
+      </ul>
+
+      <h2>IEO SALE:</h2>
+      <ul>
+        <li>Total Supply: 700,000,000</li>
+        <li>IEO Sale Allocation: 15% of Total Supply = 105,000,000</li>
+        <li>Token Sale Phases:</li>
+        <ul>
+          <li>Phase 1: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0700</li>
+            <li>Start Date: 2/16/2024</li>
+            <li>End Date: 2/20/2024</li>
+          </ul>
+          <li>Phase 2: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0800</li>
+            <li>Start Date: 2/21/2024</li>
+            <li>End Date: 2/25/2024</li>
+          </ul>
+          <li>Phase 3: 35,000,000 tokens</li>
+          <ul>
+            <li>Price per Token: $0.0900</li>
+            <li>Start Date: 2/26/2024</li>
+            <li>End Date: 3/1/2024</li>
+          </ul>
+        </ul>
+      </ul>
+
+      <h2>Public Exchange Listing</h2>
+      <ul>
+        <li>Token Sale Price on Exchange Listing: $0.1000 per token</li>
+        <li>Exchange Listing Date: March 15, 2024</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TokenSalesInfo;
